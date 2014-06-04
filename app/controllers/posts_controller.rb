a659031120e23e5f2c4f5c4bf8453c96120951b1
Tikky Shiwala <tshiwala@constantcontact.com>
2014-06-04 11:47:03 -0400

    final

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save!
+      redirect_to user_post_path(current_user, @post)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit!.merge(user_id: current_user.id)
+  end
+end
